c86780b63a97ee818545ce782c92f4201982eef9
Nishit <nishitdjoshi@gmail.com>
2021-03-25 00:58:16 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Router, Route, Switch } from "react-router";
+import { Link } from 'react-router-dom';
+
+import Home   from "./components/Home";
+import Services from "./components/Services";
+import Career from "./components/Career";
+import Contact from "./components/Contact";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <Router>
+      <div>
+        <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/carees">Careers</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+
+        <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/services">
+              <Services />
+            </Route>
+            <Route path="/careers">
+              <Career />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+        </Switch>
+        
+      </div>
+    </Router> 
+      </div>
+ 
     </div>
   );
 }
